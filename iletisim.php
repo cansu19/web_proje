9efcb7689c6def37c3825d7e3969040963ba7c71
Cansu Arslanalp <79985594+cansu19@users.noreply.github.com>
2022-05-17 10:29:34 +0300

    Update iletisim.php

--- iletisim.php
+++ iletisim.php
@@ -1,108 +1,63 @@
 <!doctype html>
 <html lang="en">
   <head>
+    <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="https://fonts.googleapis.com/css2?family=Quicksand&display=swap" rel="stylesheet">
+
     <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-wEmeIV1mKuiNpC+IOBjI7aAzPcEZeedi5yW5f2yOq55WWLwNGmvvx4Um1vskeMj0" crossorigin="anonymous">
     <link rel="stylesheet" href ="css/style.css">
-    <title>İlgi Alanlarım</title>
+    <script src="deneme.js"></script>
+    <title>İletisim</title>
   </head>
   <body>
     <nav class="navbar navbar-expand-lg arka">
-        <div class="container-fluid arka">
-          <a class="navbar-brand menu" href="#">ŞEHRİM VE BEN</a>
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="collapse navbar-collapse justify-content-end fixed-top deneme2" id="navbarNav">
-            <ul class="navbar-nav">
-              <li class="nav-item">
-                <a class="nav-link active menu" href="index.html" aria-current="page">Ana Sayfa</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link menu" href="sehrim.html">Şehrim</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link menu" href="iletisim.html">İletişim</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link menu" href="mirasimiz.html">Mirasımız</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link menu" href="login.html">Login</a>
-              </li>
-            </ul>
-          </div>
+      <div class="container-fluid arka">
+        <a class="navbar-brand menu" href="#">ŞEHRİM VE BEN</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse justify-content-end fixed-top deneme2" id="navbarNav">
+          <ul class="navbar-nav">
+            <li class="nav-item">
+              <a class="nav-link active menu" aria-current="page" href="#">Ana Sayfa</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link menu" href="sehrim.html">Şehrim</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link menu" href="iletisim.html">İletişim</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link menu" href="mirasimiz.html">Mirasımız</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link menu" href="login.html">Login</a>
+            </li>
+          </ul>
         </div>
-      </nav> 
-       <h1 class="text-center">İLGİ ALANLARIM</h1>
-      <section class= "p-3 text-center">
-        <div class="container arka3">
-          <h4>YANGIN</h4>
-     <p>Yerden göğe haksız bu duvar, ardında bir şekilde hayat var
-         <br>Gör her yangından, binbir renkten tek duman çıkar
-         <br>Canın yanmaz eğer bilmesen
-         <br>Orta yerde savaştan
-         <br>Eser yok, denilen küllük ormanda
-         <br>Yanan, sönen canımız var
-		 <br>(sevdiğim bir parçadır)
-     </p>
-    </div> 
-</section>
-<div class="app">
-  <div class="header">
-    <p>Açık hava konserleri için lazım olabilir :)</p>
-      <h1>Hava Durumu</h1>
-      <input type="text" id="searchBar" placeholder="Şehir giriniz...">
-  </div>
-  <div class="content">
-      <div class="city">Karamürsel,Tr </div>
-      <div class="temp">19°C</div>
-      <div class="desc">Güneşli</div>
-  </div>
-</div>
-<div class="row row-cols-1 row-cols-md-3 g-4">
-  <div class="col">
-    <div class="card h-100 card">
-      <img src="jpg/konser1.jpg" class="card-img-top" alt="...">
-      <div class="card-body paragraf">
-          <p class="card-text">Konserlere gitmeyi çok seviyorum, ilgi alanlarımın başında tabiki müzik var.</p>
-          </p>
-          <a href="https://www.biletix.com/anasayfa/TURKIYE/tr" class="btn btn-primary">Bilet için tıklayabilirsiniz</a>
       </div>
+    </nav>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-p34f1UUtsS3wqzfto5wAAmdvj+osOnFyQFpp4Ua3gs/ZVWx6oOypYoCJhGGScy+8" crossorigin="anonymous"></script>
+    <div class="arkaPlan">
+      <h3 class="text-center">Bilgileriniz iletildi!
+      </h3>
+<?php
+echo "AD:".$_GET["name"]."<br>";
+echo "SOYAD:".$_GET["subject"]."<br>";
+echo "EMAİL:".$_GET["email"]."<br>";
+echo "Cinsiyet:".$_GET["cinsiyet"]."<br>";
+echo "Okullar:";
+if(isset($_GET["ilk"])) echo "İlkokul-";
+if(isset($_GET["orta"])) echo "Ortaokul-";
+if(isset($_GET["lise"])) echo "Lise-";
+if(isset($_GET["uni"])) echo "Üniversite";
+echo "<br>";
+echo "BÖLGE:".$_GET["bolge"]."<br>";
+?>
     </div>
-  </div>
-    <div class="col">
-        <div class="card h-100 card">
-          <img src="jpg/gezi.jfif" class="card-img-top" alt="...">
-          <div class="card-body paragraf">
-            <p class="card-text">Gezi turlarına katılmaya bayılıyorum, özellikle arkadaşlarla gidilince daha da bir keyifli oluyor.</p>
-            <a href="https://www.jollytur.com/kultur-turlari" class="btn btn-primary">İlginizi çekebilecek turlar</a>
-          </div>
-        </div>
-      </div>
-      <div class="col">
-        <div class="card h-100 card">
-          <img src="jpg/cook.jfif" class="card-img-top" alt="...">
-          <div class="card-body paragraf">
-              <p class="card-text">Daha önce hiç katılmadım fakat katılmayı çok istediğim Cooking Workshopları</p>
-              </p>
-              <a href="https://www.tripadvisor.com/Attractions-g293974-Activities-c36-t203-Istanbul.html" class="btn btn-primary">Cooking Classes in İstanbul</a>
-          </div>
-        </div>
-      </div>
-      </div>
-      <script src="script.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
-    <div class="arka text-light text-center ">
-      <p class= "align-middle p-3 mb-0">
-        <span>Tüm hakları saklıdır 2022</span><br>
-        <span>Designed by <b>Cansu Arslanalp</b></span><br>
-      </p>
-    
-    </div>
-  
   </body>
 </html>
